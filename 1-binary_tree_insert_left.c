08818211e227565edbaa12849839cd47d45837d6
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-11-01 10:32:08 -0700

    insert at left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -26,12 +26,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
-		parent->left = new_node;
 	}
-	else
-	{
-		parent->left = new_node;
-		new_node->left = NULL;
-	}
+	parent->left = new_node;
 	return (new_node);
 }
